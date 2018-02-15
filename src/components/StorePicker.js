cce26d1f4c2490039782bea4b41aa75f9a9b45da
Gabriel Diaz <gdiaz1410@gmail.com>
2018-02-14 22:28:31 -0600

    binding time # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // option 1**
+  // constructor() {
+  //   super(); // creates a react component. Allows extra content to this component
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(e) {
+    e.preventDefault();
+    console.log('URL changed');
+    console.log(this.storeInput);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
