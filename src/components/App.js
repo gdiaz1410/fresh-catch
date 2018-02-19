2f007823b4b41b051459908599787072017077ea
Gabriel Diaz <gdiaz1410@gmail.com>
2018-02-18 22:57:46 -0600

    intro to state # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,30 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+// *** App will be our parent component where we hold state
+
 class App extends React.Component {
+  constructor() {
+    super();
+    // ** will bind method to the App
+    this.addFish = this.addFish.bind(this);
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update state
+    const fishes = {...this.state.fishes};
+    // add in new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    // fishes state has changed, here is the updated state, which is our const fishes
+    this.setState({ fishes })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +34,7 @@ class App extends React.Component {
           <Header theTagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
