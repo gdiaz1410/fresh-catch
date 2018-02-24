89b03ca0cdd49077d9d9d7694433ae71a554258d
Gabriel Diaz <gdiaz1410@gmail.com>
2018-02-23 23:13:55 -0600

    updating state # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
     // ** will bind method to the App
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     // get initial state
     this.state = {
       fishes: {},
@@ -39,6 +40,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // take copy of our state
+    const order = {...this.state.order};
+    // update or add new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -47,7 +57,7 @@ class App extends React.Component {
           <ul className="list-of-fishes">
             {
               Object.keys(this.state.fishes)
-              .map(key =>  <Fish key={key} details={this.state.fishes[key]} />)
+              .map(key =>  <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
