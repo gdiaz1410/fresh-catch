ccb3224ac42ef77cc891345728197ca73fa75561
Gabriel Diaz <gdiaz1410@gmail.com>
2018-02-27 22:42:06 -0600

    setup firebase # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyC61rSjfuzvf_sOHISxGXyZfHFcObvkEn0",
+  authDomain: "catch-of-the-day-gabe-diaz.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-gabe-diaz.firebaseio.com",
+});
+
+export default base;
