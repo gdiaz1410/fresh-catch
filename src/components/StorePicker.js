4f8b9f68960aff13842b044471e5ed5e37e7d4d6
Gabriel Diaz <gdiaz1410@gmail.com>
2018-03-18 22:41:37 -0500

    react refs # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  // option 1**
-  // constructor() {
-  //   super(); // creates a react component. Allows extra content to this component
-  //   this.goToStore = this.goToStore.bind(this);
-  // }
+  myInput = React.createRef();
 
-  goToStore(e) {
-    e.preventDefault();
-    console.log('URL changed');
-    // 1. get text from field
-    const storeId = this.storeInput.value
-    // 2. URL from / to /store/:storeid
-    this.context.router.transitionTo(`/store/${storeId}`);
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
   }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     )
@@ -29,8 +29,8 @@ class StorePicker extends React.Component {
 }
 
 // import router
-StorePicker.contextTypes = {
-  router: React.PropTypes.object
-}
+// StorePicker.contextTypes = {
+//   router: React.PropTypes.object
+// }
 
 export default StorePicker;
